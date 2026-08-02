0402c8ae2ac73bb7b3e76b161f4cc55e67edd210
Jorge Brunal Pérez <diniremix@gmail.com>
2026-08-02 17:43:56 -0500

    fix key evets, added Dino and Obstacle

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,190 @@ use ggez::conf;
 use ggez::event::{self, EventHandler};
 use ggez::graphics;
 use ggez::graphics::{Canvas, Color, DrawMode, DrawParam, GraphicsContext, Image, Mesh, Rect};
+use ggez::input::keyboard::{KeyCode, KeyInput};
 use ggez::{Context, GameResult, glam};
 use glam::Vec2;
+//use winit::keyboard::{Key, NamedKey};
+use winit::keyboard::{Key, NamedKey};
 
 const WIDTH: f32 = 800.0;
 const HEIGHT: f32 = 600.0;
 
-// el struct principal
-struct MyGame {}
+struct Dino {
+    image: Image,
+    w: f32,
+    h: f32,
+    position: Vec2,
+    speed: Vec2,
+    hitbox: Rect,
+}
 
-impl MyGame {
-    pub fn new(ctx: &mut Context) -> GameResult<MyGame> {
-        Ok(MyGame {})
+impl Dino {
+    pub fn new(ctx: &mut Context) -> GameResult<Self> {
+        let image = Image::from_path(ctx, "/dino.png")?;
+        let w = image.width() as f32;
+        let h = image.height() as f32;
+
+        Ok(Self {
+            image,
+            w,
+            h,
+            position: Vec2::new(0., 0.),
+            speed: Vec2::new(0., 0.),
+            hitbox: Rect::new(0.0, 0.0, w, h),
+        })
+    }
+
+    pub fn get_rect(&self) -> Rect {
+        Rect::new(self.position.x, self.position.y, self.w, self.h)
+    }
+
+    pub fn draw_hitbox(&self, canvas: &mut Canvas, gfx: &mut GraphicsContext) {
+        let hb = self.get_rect();
+        let hitbox_mesh = Mesh::new_rectangle(gfx, DrawMode::stroke(2.0), hb, Color::RED)
+            .expect("Error al crear rectángulo de debug");
+
+        canvas.draw(&hitbox_mesh, DrawParam::default());
+    }
+
+    pub fn update(&mut self, dt: f32, screen_size: glam::Vec2) {
+        self.position += self.speed * dt;
+
+        self.set_in_window(dt, screen_size.x, screen_size.y);
+    }
+
+    fn draw(&self, canvas: &mut graphics::Canvas) -> GameResult {
+        let params = DrawParam::default().dest(Vec2::new(self.position.x, self.position.y));
+
+        canvas.draw(&self.image, params);
+        Ok(())
+    }
+
+    /// mantiene el dinosaurio dentro de la
+    // ventana principal
+    pub fn set_in_window(&mut self, dt: f32, x: f32, y: f32) {
+        let w = self.w;
+        let h = self.h;
+
+        self.position.x = self.position.x.clamp(0.0, x - w);
+        self.position.y = self.position.y.clamp(0.0, y - h);
+
+        // mantener el hitbox sincronizado
+        self.hitbox.x = self.position.x;
+        self.hitbox.y = self.position.y;
     }
 }
 
-impl EventHandler for MyGame {
+struct Obstacle {
+    rect: Rect,
+    color: Color,
+}
+
+impl Obstacle {
+    pub fn new(obst: Rect, color: Color) -> Self {
+        Self { rect: obst, color }
+    }
+
+    pub fn get_rect(&self) -> Rect {
+        self.rect
+    }
+
+    pub fn draw_hitbox(&self, canvas: &mut Canvas, gfx: &mut GraphicsContext) {
+        let hb = self.get_rect();
+        let hitbox_mesh = Mesh::new_rectangle(gfx, DrawMode::stroke(2.0), hb, Color::GREEN)
+            .expect("Error al crear rectángulo de debug");
+
+        canvas.draw(&hitbox_mesh, DrawParam::default());
+    }
+
+    fn draw(&self, canvas: &mut graphics::Canvas, gfx: &mut GraphicsContext) -> GameResult {
+        let mesh =
+            Mesh::new_rectangle(gfx, DrawMode::fill(), self.rect, self.color).expect("Error al crear el rectángulo");
+
+        let params = DrawParam::default();
+        canvas.draw(&mesh, params);
+        Ok(())
+    }
+}
+
+// el struct principal
+struct MainState {
+    dino: Dino,
+    obstacle: Obstacle,
+}
+
+impl MainState {
+    pub fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let dino = Dino::new(ctx)?;
+        let screen_size = ctx.gfx.drawable_size();
+        let screen_width: glam::Vec2 = screen_size.into();
+
+        let rectangle = Rect::new(screen_width.y / 2.0 + 50.0, 150.0, 220.0, 120.0);
+        let obstacle = Obstacle::new(rectangle, Color::BLACK);
+
+        Ok(MainState { dino, obstacle })
+    }
+}
+
+impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let dt = ctx.time.delta().as_secs_f32();
+        let screen_size = ctx.gfx.drawable_size();
+
+        self.dino.update(dt, screen_size.into());
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
 
+        self.dino.speed = Vec2::new(0., 0.);
+        self.dino.draw(&mut canvas)?;
+        self.obstacle.draw(&mut canvas, &mut ctx.gfx)?;
+
+        let is_colliding = self.dino.get_rect().overlaps(&self.obstacle.get_rect());
+
+        if is_colliding {
+            self.dino.draw_hitbox(&mut canvas, &mut ctx.gfx);
+            self.obstacle.draw_hitbox(&mut canvas, &mut ctx.gfx);
+            //draw_text(format!("objetos colisionando!").as_str(), 0., 40., 24., RED);
+        }
+
         canvas.finish(ctx)
+    }
+
+    fn key_down_event(&mut self, ctx: &mut Context, input: KeyInput, _repeat: bool) -> GameResult {
+        match input.event.logical_key {
+            Key::Named(NamedKey::ArrowRight) => {
+                println!("KeyCode::Right");
+                self.dino.speed.x = 100.0;
+            }
+            Key::Named(NamedKey::ArrowLeft) => {
+                println!("KeyCode::Left");
+                self.dino.speed.x = -100.0;
+            }
+            Key::Named(NamedKey::ArrowUp) => {
+                println!("KeyCode::Up");
+                self.dino.speed.y = -100.0;
+            }
+            Key::Named(NamedKey::ArrowDown) => {
+                println!("KeyCode::Down");
+                self.dino.speed.y = 100.0;
+            }
+            Key::Named(NamedKey::Escape) => {
+                println!("saliendo...");
+                //ctx.request_quit();
+            }
+            _ => {}
+        }
+        Ok(())
     }
 }
 
 fn main() -> GameResult {
+    let resource_dir = std::path::PathBuf::from("/home/helio/projects/rs-work/ggez-hitbox");
+
     let cb = ggez::ContextBuilder::new("HitBox", "Helio Studio Games")
         .window_setup(conf::WindowSetup::default().title("ggez :: HitBox"))
         .window_mode(
@@ -40,12 +196,13 @@ fn main() -> GameResult {
                 .dimensions(WIDTH, HEIGHT)
                 .maximized(false)
                 .resizable(false),
-        );
+        )
+        .add_resource_path(&resource_dir);
 
     let (mut ctx, event_loop) = cb.build()?;
 
-    //let state = MyGame::new(&mut ctx);
-    let state = MyGame::new(&mut ctx)?;
+    //let state = MainState::new(&mut ctx);
+    let state = MainState::new(&mut ctx)?;
 
     event::run(ctx, event_loop, state)
 }

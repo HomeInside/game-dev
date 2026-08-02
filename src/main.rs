a6438f61b87cfbabfbb7eba45aa6ad158844190e
Jorge Brunal Pérez <diniremix@gmail.com>
2026-08-02 12:51:08 -0500

    added Obstacle, and many methods and bug fixeds

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use macroquad::prelude::*;
 use macroquad::time::{draw_fps, get_frame_time};
 use macroquad::window;
 
-const WIDTH: f32 = 800.0;
-const HEIGHT: f32 = 600.0;
+const WIDTH: i32 = 800;
+const HEIGHT: i32 = 600;
 
 #[allow(dead_code)]
 const PHYSICS_DT: f32 = 1.0 / 120.0; //60
@@ -14,8 +14,8 @@ struct Dino {
     image: Texture2D,
     w: f32,
     h: f32,
-    speed: Vec2,
     position: Vec2,
+    speed: Vec2,
     hitbox: Rect,
 }
 
@@ -27,16 +27,26 @@ impl Dino {
             image,
             w,
             h,
-            speed: vec2(0., 0.),
             position: vec2(0., 0.),
+            speed: vec2(0., 0.),
             hitbox: Rect::new(0.0, 0.0, w, h),
         }
     }
-    pub fn draw(&self) {
-        //draw_texture(&self.image, 0., 0., WHITE);
-        draw_texture(&self.image, self.position.x, self.position.y, WHITE);
+
+    pub fn get_rect(&self) -> Rect {
+        Rect::new(self.position.x, self.position.y, self.w, self.h)
     }
 
+    pub fn draw_hitbox(&self) {
+        let hb = self.get_rect();
+        draw_rectangle_lines(hb.x, hb.y, hb.w, hb.h, 4.0, RED);
+    }
+
+    pub fn draw(&self) {
+        draw_texture(&self.image, self.position.x, self.position.y, WHITE);
+    }
+    /// mantiene el dinosaurio dentro de la
+    // ventana principal
     pub fn set_in_window(&mut self, dt: f32) {
         self.position += self.speed * dt;
 
@@ -46,28 +56,40 @@ impl Dino {
         self.position.x = self.position.x.clamp(0.0, screen_width() - w);
         self.position.y = self.position.y.clamp(0.0, screen_height() - h);
 
-        //Mantener el hitbox sincronizado
+        // mantener el hitbox sincronizado
         self.hitbox.x = self.position.x;
         self.hitbox.y = self.position.y;
-        self.draw_hitbox();
+    }
+}
+
+struct Obstacle {
+    rect: Rect,
+    color: Color,
+}
+
+impl Obstacle {
+    pub fn new(obst: Rect, color: Color) -> Self {
+        Self { rect: obst, color }
     }
 
     pub fn get_rect(&self) -> Rect {
-        Rect::new(self.position.x, self.position.y, self.w, self.h)
+        self.rect
     }
 
     pub fn draw_hitbox(&self) {
-        let hb = self.get_rect();
-        //draw_rectangle_lines(self.hitbox.x, self.hitbox.y, self.hitbox.w, self.hitbox.h, 4.0, GREEN);
-        draw_rectangle_lines(hb.x, hb.y, hb.w, hb.h, 4.0, GREEN);
+        draw_rectangle_lines(self.rect.x, self.rect.y, self.rect.w, self.rect.h, 4.0, GREEN);
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, self.color);
     }
 }
 
 fn window_conf() -> window::Conf {
     window::Conf {
         window_title: "macroquad :: HitBox".to_owned(),
-        window_width: WIDTH as i32,
-        window_height: HEIGHT as i32,
+        window_width: WIDTH,
+        window_height: HEIGHT,
         ..Default::default()
     }
 }
@@ -77,16 +99,25 @@ async fn main() {
     let dino_texture: Texture2D = load_texture("./dino.png").await.unwrap();
     dino_texture.set_filter(FilterMode::Nearest);
     let mut dino = Dino::new(dino_texture);
-    
+
+    let rectangle = Rect::new(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0);
+    let obstacle = Obstacle::new(rectangle, BLACK);
+
     loop {
         let dt = get_frame_time();
 
-        clear_background(LIGHTGRAY); //BLACK
+        clear_background(WHITE); //BLACK
 
         dino.speed = vec2(0.0, 0.0);
-
         dino.draw();
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, BLACK);
+
+        obstacle.draw();
+        let is_colliding = dino.get_rect().overlaps(&obstacle.get_rect());
+
+        if is_colliding {
+            dino.draw_hitbox();
+            obstacle.draw_hitbox();
+        }
 
         if is_key_down(KeyCode::Right) {
             //println!("KeyCode::Right");

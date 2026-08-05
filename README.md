1ffb797cbaa5e0532038108dd29385ba621761b2
Jorge Brunal Pérez <diniremix@gmail.com>
2026-08-04 19:56:32 -0500

    fix docs, fix imports(avoid prelude for now...)

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Sprites y animaciones con macroquad
 
+## notas
+- Sprite: En el mundo de los videojuegos, el "Sprite" (o Asset) es todo el archivo de imagen. Es el objeto completo que ves. Por ejemplo, `male_hero-run.png` es un Sprite Sheet (Hoja de Sprites).
+
+- Celda, Cuadro ó Frame: Cada uno de los dibujitos individuales dentro de esa hoja se llama técnicamente un Frame (cuadro), ó a veces Celda (como en la animación).
+
 ## assets
 la carpeta `assets` contiene archivos png con transparencias.
 
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 //use macroquad::prelude::*;
-use macroquad::color::{BLACK, Color, WHITE, colors::LIGHTGRAY};
-use macroquad::math::Rect;
-use macroquad::window;
+use macroquad::color::{BLACK, WHITE};
+//use macroquad::color::{Color, colors::LIGHTGRAY};
+use macroquad::math::{Rect, vec2};
+//use macroquad::window;
 //use macroquad::shapes::draw_circle;
 use macroquad::input::{KeyCode, is_key_down};
-use macroquad::math::vec2;
 use macroquad::text::draw_text;
 use macroquad::texture::{DrawTextureParams, Texture2D, draw_texture_ex, load_texture};
-use macroquad::time::{draw_fps, get_fps, get_frame_time};
+//use macroquad::time::{draw_fps, get_fps};
+use macroquad::time::get_frame_time;
 use macroquad::window::{clear_background, next_frame};
 
 #[macroquad::main("Animation Test")]
 async fn main() {
-    // 1. Cargar la textura
+    // 1. cargar la textura(el Sprite Sheet)
     let texture: Texture2D = load_texture("assets/male_hero-run.png").await.unwrap();
 
-    // 2. Calcular datos básicos
+    // 2. calcular datos básicos
     let sprite_size = 128.0; // todos miden 128x128, falta revisar
     let total_frames = (texture.width() / sprite_size) as usize; // 1280 / 128 = 10
-    let frame_speed = 0.1; // Cambia la celda del sprite cada 0.1 segundos
+    let frame_speed = 0.1; // cambia el cuadro ó frame cada 0.1 segundos
 
     let mut current_frame = 0;
     let mut timer = 0.0;
     let mut position = vec2(300.0, 250.0);
-    let mut facing_right = true;
+    let mut facing_right = true; // si el jugador mira a la derecha o izda
 
     loop {
         clear_background(BLACK);
@@ -31,6 +32,7 @@ async fn main() {
 
         // 3. Manejo de Input
         let mut speed = 0.0;
+
         if is_key_down(KeyCode::Right) {
             speed = 200.0;
             facing_right = true;
@@ -53,10 +55,11 @@ async fn main() {
         }
 
         // 5. Cálculo del área de recorte de la textura
+        // aqui se obtiene el frame del Sprite Sheet
         let source_x = current_frame as f32 * sprite_size;
         let source_rect = Rect::new(source_x, 0.0, sprite_size, texture.height());
 
-        // 6. Dibujar la textura recortada (usando DrawTextureParams)
+        // 6. Dibujar la textura recortada (el frame, usando DrawTextureParams)
         draw_texture_ex(
             &texture,
             position.x,
@@ -64,7 +67,7 @@ async fn main() {
             WHITE,
             DrawTextureParams {
                 source: Some(source_rect),
-                flip_x: !facing_right, // Gira la celda el sprite si va a la izquierda
+                flip_x: !facing_right, // gira el frame si va a la izquierda
                 ..Default::default()
             },
         );

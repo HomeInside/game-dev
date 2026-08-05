73299335fae9fa10f8506a19f0324f028be6a927
Jorge Brunal Pérez <diniremix@gmail.com>
2026-08-04 21:52:33 -0500

    :new: added constant, window_conf, load textures to jump and fall, detect jump, change animation, apply floor and gravity, added some debug info text

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,28 @@ use macroquad::color::{BLACK, WHITE};
 use macroquad::math::{Rect, vec2};
 //use macroquad::window;
 //use macroquad::shapes::draw_circle;
-use macroquad::input::{KeyCode, is_key_down};
+use macroquad::input::{KeyCode, is_key_down, is_key_pressed};
 use macroquad::text::draw_text;
 use macroquad::texture::{DrawTextureParams, Texture2D, draw_texture_ex, load_texture};
 //use macroquad::time::{draw_fps, get_fps};
 use macroquad::time::get_frame_time;
-use macroquad::window::{clear_background, next_frame};
+use macroquad::window::{self, clear_background, next_frame, screen_height};
 
-#[macroquad::main("Animation Test")]
+const WIDTH: i32 = 800;
+const HEIGHT: i32 = 600;
+const GRAVITY: f32 = 600.0; // La fuerza de gravedad
+const JUMP_SPEED: f32 = -350.0; // velocidad inicial del salto(Empuje inicial hacia arriba
+
+fn window_conf() -> window::Conf {
+    window::Conf {
+        window_title: "macroquad :: sprite-anim".to_owned(),
+        window_width: WIDTH,
+        window_height: HEIGHT,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     // 1. cargar la textura(el Sprite Sheet)
     // para este ejemplo todos los Sprite Sheet
@@ -19,6 +33,8 @@ async fn main() {
     let texture_walk: Texture2D = load_texture("assets/male_hero-walk.png").await.unwrap();
     let texture_run: Texture2D = load_texture("assets/male_hero-run.png").await.unwrap();
     let texture_idle: Texture2D = load_texture("assets/male_hero-idle.png").await.unwrap();
+    let texture_jump: Texture2D = load_texture("assets/male_hero-jump.png").await.unwrap();
+    let texture_fall: Texture2D = load_texture("assets/male_hero-fall.png").await.unwrap();
 
     let frame_width = texture_walk.width();
 
@@ -35,6 +51,10 @@ async fn main() {
     // si se ha movido
     let mut was_moving = false;
 
+    // física y manejo de gravedad
+    let mut vel_y = 0.0;
+    let mut is_grounded = true;
+
     loop {
         clear_background(WHITE);
         let dt = get_frame_time();
@@ -50,6 +70,14 @@ async fn main() {
         if is_key_down(KeyCode::Left) {
             speed = -60.0;
             facing_right = false;
+        }
+
+        //TODO Detectar el salto
+        if is_key_pressed(KeyCode::Space) && is_grounded {
+            vel_y = JUMP_SPEED;
+            is_grounded = false;
+            current_frame = 0; // Reiniciamos la animación de salto para que empiece por el principio
+            timer = 0.0;
         }
 
         let is_moving = is_key_down(KeyCode::Right) || is_key_down(KeyCode::Left);
@@ -71,6 +99,7 @@ async fn main() {
         // segun si camina o corre.
         // Si me muevo, corro ó camino.
         // Si NO me muevo, hago `idle` (resposo/quieto)
+        /*
         let (texture, frame_speed, speed) = if is_key_down(KeyCode::Right) || is_key_down(KeyCode::Left) {
             // Si estamos moviendo el jugador, decidimos entre correr ó caminar
             if is_key_down(KeyCode::LeftShift) {
@@ -87,8 +116,54 @@ async fn main() {
             // que el jugador "respire"
             (&texture_idle, 0.20, speed)
         };
+        */
+        // TODO start
+        // Elegir textura, velocidad de animación y velocidad de movimiento
+        let (texture, frame_speed, speed) = if !is_grounded {
+            // ESTAMOS EN EL AIRE
+            if vel_y < 0.0 {
+                // Subiendo (Salto)
+                // Usamos la animación de salto. La ponemos rápida (0.06) y SIN bucle (se congelará al final)
+                (&texture_jump, 0.06, speed)
+            } else {
+                // Bajando (Caída)
+                // Usamos la animación de caída. (0.10 para que se vea natural)
+                (&texture_fall, 0.10, speed)
+            }
+        } else {
+            // ESTAMOS EN EL SUELO
+            if is_key_down(KeyCode::Right) || is_key_down(KeyCode::Left) {
+                // Movimiento horizontal
+                if is_key_down(KeyCode::LeftShift) {
+                    let n_speed = if facing_right { speed + 160.0 } else { speed - 160.0 };
+                    (&texture_run, 0.08, n_speed)
+                } else {
+                    (&texture_walk, 0.10, speed)
+                }
+            } else {
+                // Reposo
+                (&texture_idle, 0.20, speed)
+            }
+        };
+        // TODO end
 
+        // TODO
+        // s1. Aplicar gravedad al eje Y
+        vel_y += GRAVITY * dt;
+
+        // s2. Mover en Y
+        position.y += vel_y * dt;
+        //
         position.x += speed * dt;
+
+        // TODO
+        // s3. Detectar el suelo (parte inferior de la ventana)
+        if position.y + sprite_size >= screen_height() {
+            position.y = screen_height() - sprite_size; // 128 // Ajustamos al borde exacto
+            //position.y = screen_height() - 100.0; // Ajustamos al borde exacto
+            vel_y = 0.0;
+            is_grounded = true;
+        }
 
         // 4. Lógica de la animación (cambiar el frame)
         timer += dt;
@@ -119,7 +194,15 @@ async fn main() {
         );
 
         draw_text("Usa FLECHA IZQUIERDA y DERECHA para moverte", 0.0, 20.0, 25.0, BLACK);
-        draw_text(&format!("Frame actual: {}", current_frame), 0.0, 50.0, 25.0, BLACK);
+        draw_text(&format!("Frame actual: {}", current_frame), 0.0, 35.0, 25.0, BLACK);
+        draw_text(format!("toca el suelo: {}", is_grounded).as_str(), 0., 50., 24., BLACK);
+        draw_text(
+            format!("Player X: {:.1}, y: {:.1}", position.x, position.y).as_str(),
+            0.,
+            65.,
+            24.,
+            BLACK,
+        );
 
         next_frame().await;
     }

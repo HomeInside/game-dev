313befc810d94236892bbffc0c41df0a26cf2110
Jorge Brunal Pérez <diniremix@gmail.com>
2026-08-18 19:00:53 -0500

    initial box example

---
+++ macro-box/src/main.rs
@@ -0,0 +1,28 @@
+// hide console window on Windows in release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use macroquad::prelude::*;
+use macroquad::window::{self, next_frame};
+
+fn window_conf() -> window::Conf {
+    window::Conf {
+        window_title: "macro::box".to_owned(),
+        window_width: 800,
+        window_height: 600,
+        high_dpi: true,
+        fullscreen: false,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() -> Result<(), macroquad::Error> {
+    loop {
+        clear_background(WHITE);
+        let dt = get_frame_time();
+
+        next_frame().await;
+    }
+    // loop perimetral END
+} //main
